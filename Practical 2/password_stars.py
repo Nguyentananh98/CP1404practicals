7e75ff777c2db4604d7225b60a6cf20064cb5cc2
NguyenTanAnh <nguyentananh291298@gmail.com>
2022-11-14 16:10:46 +0800

    add temperature file to practical 2

--- Practical 2/password_stars.py
+++ Practical 2/password_stars.py
@@ -1,14 +1,25 @@
+"""
+CP1404
+NGUYEN TAN ANH -JC950881
+Practical_2
+"""
+
 MINIMUM_LENGTH = 8
 
 
 def main():
     password = get_password()
+    display_asterisk(password)
 
+
+def display_asterisk(password):
+    """display asterisk that have the length of the password"""
     for i in range(len(password)):
         print('*', end='')
 
 
 def get_password():
+    """check if the password have minimum required length"""
     password = input("Please enter your password: ")
     while len(password) < MINIMUM_LENGTH:
         print("THe password length must be 8 letters or more!")
