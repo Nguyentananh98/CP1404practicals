a81384ce3445fb0e7bbf8de8d1dd4faa283bbdff
NguyenTanAnh <nguyentananh291298@gmail.com>
2022-11-14 16:23:26 +0800

    refactor the score file to have 1 main function and 1 define result function

--- Practical 2/score.py
+++ Practical 2/score.py
@@ -3,18 +3,28 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
-#Fix this!
+# Fix this!
 UPPER_LEVEL = 90
 MIDDLE_LEVEL = 50
 UPPER_LIMIT = 100
 LOWER_LIMIT = 0
 
-score = float(input("Enter score: "))
-if (score < LOWER_LIMIT) and (score > UPPER_LIMIT):
-    print("Invalid score")
-elif score >= UPPER_LEVEL:
-    print("Excellent")
-elif score >= MIDDLE_LEVEL:
-    print("Passable")
-else:
-    print("Bad")
+
+def main():
+    score = float(input("Enter score: "))
+    define_result(score)
+
+
+def define_result(score):
+    """define the result of the input score"""
+    if (score < LOWER_LIMIT) and (score > UPPER_LIMIT):
+        print("Invalid score")
+    elif score >= UPPER_LEVEL:
+        print("Excellent")
+    elif score >= MIDDLE_LEVEL:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
