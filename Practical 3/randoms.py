f276ef4451e983c09a527a6c950dfad89b21b22d
NguyenTanAnh <nguyentananh291298@gmail.com>
2022-11-21 15:14:09 +0800

    finish random file

---
+++ Practical 3/randoms.py
@@ -0,0 +1,15 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+# the smallest number i saw is 5, the largest was 20
+
+print(random.randrange(3, 10, 2))  # line 2
+# the smallest number i saw is 3, the largest is 9
+# line 2 cannot produce a 4
+
+print(random.uniform(2.5, 5.5))  # line 3
+# the smallest i can saw is 3.05, the highest is 5.49
+
+# write code
+print(random.randint(1, 101))
+
