d9660e3380a29fe5e76c740d7b8176237fac4d77
NguyenTanAnh <nguyentananh291298@gmail.com>
2022-11-21 15:46:06 +0800

    finish question 3 part 1 in files.py

--- Practical 3/files.py
+++ Practical 3/files.py
@@ -6,18 +6,29 @@ JC950881
 
 # question 1
 
-OUTPUT_FILE = 'name.txt'
-
-name = input('Please enter your name: ')
-out_file = open(OUTPUT_FILE, 'w')
-print(name, file=out_file)
-out_file.close()
-
-# question 2
-INPUT_FILE = 'name.txt'
-
-in_file = open(INPUT_FILE, 'r')
-name = in_file.read()
-print(f'Your name is {name}')
+# OUTPUT_FILE = 'name.txt'
+#
+# name = input('Please enter your name: ')
+# out_file = open(OUTPUT_FILE, 'w')
+# print(name, file=out_file)
+# out_file.close()
+#
+# # question 2
+# INPUT_FILE = 'name.txt'
+#
+# in_file = open(INPUT_FILE, 'r')
+# name = in_file.read()
+# print(f'Your name is {name}')
 
 # question 3
+# OUTPUT_FILE = 'numbers.txt'
+# out_file = open(OUTPUT_FILE, 'w')
+# numbers = [17, 42, 400]
+# for number in numbers:
+#     print(number, file=out_file)
+# out_file.close()
+
+"""open numbers.txt and read the first 2 numbers"""
+INPUT_FILE = 'numbers.txt'
+in_file = open(INPUT_FILE, 'w')
+
