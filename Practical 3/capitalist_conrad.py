978c38b37367f0afa13a3966e9582ad8dd30cd30
NguyenTanAnh <nguyentananh291298@gmail.com>
2022-11-21 15:29:17 +0800

    do task 3 capitalist_conrad.py

--- Practical 3/capitalist_conrad.py
+++ Practical 3/capitalist_conrad.py
@@ -14,10 +14,12 @@ MAX_DECREASE = 0.05  # 5%
 MIN_PRICE = 1.0
 MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
+OUTPUT_FILE = 'out_put_text'
 
 price = INITIAL_PRICE
 day_count = 1
-print(f"Starting price: ${price:,.2f}")
+out_file = open(OUTPUT_FILE, 'w')
+print(f"Starting price: ${price:,.2f}", file=out_file)
 
 while MIN_PRICE <= price <= MAX_PRICE:
     price_change = 0
@@ -33,7 +35,11 @@ while MIN_PRICE <= price <= MAX_PRICE:
         price_change = random.uniform(-MAX_DECREASE, 0)
 
     price *= (1 + price_change)
-    print(f'On day {day_count} price is: ${price:,.2f}')
+
+    print(f'On day {day_count} price is: ${price:,.2f}', file=out_file)
     day_count += 1
+
+
+out_file.close()
 
 
