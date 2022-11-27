63f0020ebc01e32a4eec57e296347a82d4612c22
NguyenTanAnh <nguyentananh291298@gmail.com>
2022-11-28 07:26:58 +0800

    add total income.py

---
+++ Practical 4/lists_warmup.py
@@ -0,0 +1,20 @@
+"""
+CP1404 - Programming 2
+NGUYEN TAN ANH - JC950881
+Practical 4
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# 1.
+numbers[0] = 10
+
+# 2.
+numbers[-1] = 1
+
+# 3.
+print(numbers[2:])
+
+# 4.
+print(9 in numbers)
+
