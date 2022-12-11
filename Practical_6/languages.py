49cd4881213df9788d7888c47393b3e272a4e285
NguyenTanAnh <nguyentananh291298@gmail.com>
2022-12-12 07:57:16 +0800

    finish languages.py and programming_language.py

---
+++ Practical_6/languages.py
@@ -0,0 +1,23 @@
+"""
+Practical 06
+NGUYEN TAN ANH
+"""
+
+from Practical_6.programming_language import ProgrammingLanguage
+
+
+def main():
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(python.is_dynamic())
+    print(python)
+    languages = [python, ruby, visual_basic]
+
+    print('The dynamically typed languages are:')
+    for language in languages:
+        if language.is_dynamic():
+            print(language.name)
+
+
+main()
---
+++ Practical_6/programming_language.py
@@ -0,0 +1,21 @@
+"""
+Practical 6
+NGUYEN TAN ANH
+"""
+
+
+class ProgrammingLanguage:
+    def __init__(self, name="", typing="", reflection=bool, year=""):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        if self.typing.lower() == "static":
+            return False
+        else:
+            return True
+
+    def __str__(self):
+        return f'{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}'
