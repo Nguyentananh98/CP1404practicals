d2f20f83b41430b5ce16be191886337acce9b2a4
NguyenTanAnh <nguyentananh291298@gmail.com>
2022-12-12 08:57:41 +0800

    add __lt__() function to the guitar class

--- Practical_6/guitar.py
+++ Practical_6/guitar.py
@@ -3,6 +3,7 @@ Practical 06
 NGUYEN TAN ANH
 """
 CURRENT_YEAR = 2022
+VINTAGE_CUTOFF = 50
 
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
@@ -11,13 +12,19 @@ class Guitar:
         self.cost = cost
 
     def __str__(self):
+        """return the information of the guitar"""
         return f'{self.name} ({self.year}) : ${self.cost}'
 
     def get_age(self):
+        """get the age of the guitar to the current year"""
         return CURRENT_YEAR - self.year
 
     def is_vintage(self):
-        if self.get_age() >= 50:
+        """check if the guitar is vintage"""
+        if self.get_age() >= VINTAGE_CUTOFF:
             return True
         else:
             return False
+
+    def __lt__(self, other):
+        return self.get_age() < other.get_age()
